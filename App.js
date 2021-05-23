d22f1b2e08833c6738338837fc5bd5214ce1d2cf
leo <leo000111444@gmail.com>
2021-05-23 15:35:02 +0800

    learning native

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Icon,
+  Text,
+  Badge,
+} from "native-base";
+import getTheme from "./native-base-theme/components";
+import material from "./native-base-theme/variables/material";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Header />
+      <Content />
+      <Footer>
+        <FooterTab>
+          <Button>
+            <Icon name="apps" />
+          </Button>
+          <Button>
+            <Icon name="camera" />
+          </Button>
+          <Button active>
+            <Icon active name="navigate" />
+          </Button>
+          <Button>
+            <Icon name="person" />
+          </Button>
+        </FooterTab>
+      </Footer>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
