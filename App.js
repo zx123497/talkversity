c409448a97df98e2c7aee0cca4aa05ac52507e81
JupiterHsieh <taipeiallen15@gmail.com>
2021-05-23 23:05:37 +0800

    Login added, app.js navigate to login page

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { AppLoading } from "expo";
 import Navigator from "./src/routes/homeStack";
 import Intro from "./src/pages/Intro/Intro";
+import Login from "./src/pages/Login";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Button } from "react-native-paper";
 const App = (props) => {
@@ -9,6 +10,7 @@ const App = (props) => {
   const handleIntroDone = () => {
     setIntro(false);
   };
-  return <>{intro ? <Intro handleDone={handleIntroDone} /> : <Navigator />}</>;
+  return <>{<Login />}</>;
+  // return <>{intro ? <Intro handleDone={handleIntroDone} /> : <Navigator />}</>;
 };
 export default App;
